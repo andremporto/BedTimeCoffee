4b9ea85552516efba677e3a9806edef0eb44e464
André Porto <andreporto@me.com>
2023-07-03 10:29:21 -0300

    Initial Commit

---
+++ CoffeeRest/CoffeeRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeRestApp.swift
+//  CoffeeRest
+//
+//  Created by André Porto on 03/07/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
